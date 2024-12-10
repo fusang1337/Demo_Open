70d5309549e4bd3aa7b6f3993718d2d32e446976
扶桑 <s1711880582@gmail.com>
2024-12-10 20:30:03 +0800

    Add files via upload

---
+++ 03-Train.py
@@ -0,0 +1,20 @@
+# 移动Arial.ttf到/root/.config/Ultralytics/Arial.ttf
+import os
+os.system("cp Arial.ttf /root/.config/Ultralytics/Arial.ttf")
+os.system("pip install ultralytics")
+
+os.system("cd /kaggle/working/")
+os.chdir("/kaggle/working/")
+
+
+#延迟启动
+import time
+time.sleep(30)
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data="02-YOLODataSet.yaml", epochs=150, imgsz=640, batch=0.9)
